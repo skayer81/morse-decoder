f33d36fd573fe4bc0054bbb9a0f07f370b5daed4
skayer81 <114329023+skayer81@users.noreply.github.com>
2023-01-24 01:29:52 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let inMorse = (letter) =>{
+        result = '';
+        while(letter.length>0){
+            switch (letter.slice(0 ,2)) {
+                case '10': {result += '.'; break;}
+                case '11': {result += '-'; break;}
+            }
+            letter = letter.slice(2);
+        }
+        return MORSE_TABLE[result];
+    }
+
+    let result = '', letter='';
+    let words = expr.split('**********');
+
+    words.forEach(word => {
+        while(word.length>0){
+            letter = word.slice(0 ,10);
+            result += inMorse(letter);
+            word = word.slice(10);
+        }
+        result += ' ';
+    });
+    return result.trim();
 }
 
 module.exports = {
